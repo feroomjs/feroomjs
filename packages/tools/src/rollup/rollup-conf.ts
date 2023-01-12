b864896bb73bfff08fe88c8f449a9db9b40e8c09
mav-rik <mav777art@gmail.com>
2023-01-12 19:13:41 +0100

    feat(tools): add more rollup plugins

--- packages/tools/src/rollup/rollup-conf.ts
+++ packages/tools/src/rollup/rollup-conf.ts
@@ -3,20 +3,34 @@ import { pkg } from "../utils"
 import { getVirtualIndex } from "../virtual-index"
 import { feroomConfPlugin } from "./feroom-plugin"
 import virtual from 'rollup-plugin-virtual'
-import typescript from 'rollup-plugin-typescript2'
 
 export interface TFeRoomRollupOptions {
     input: string
+    vue?: boolean
+    css?: string | object
     feroomConfPath?: string
     output?: string // './dist/index.js'
     plugins?: Plugin[]
 }
 
-export function createFeroomRollupConfig(options: TFeRoomRollupOptions) {
+export async function createFeroomRollupConfig(options: TFeRoomRollupOptions) {
     const plugins = []
     if (options.input.endsWith('.ts')) {
         if (!options.plugins?.find(p => ['rpt2', 'rpt', 'typescritpt', 'rollup-plugin-typescript', 'rollup-plugin-typescript2'].includes(p.name))) {
+            const typescript = (await import('rollup-plugin-typescript2')).default
             plugins.push(typescript())
+        }
+    }
+    if (options.vue) {
+        if (!options.plugins?.find(p => ['vue'].includes(p.name))) {
+            const vue = (await import('rollup-plugin-vue')).default
+            plugins.push(vue())
+        }
+    }
+    if (options.css) {
+        if (!options.plugins?.find(p => ['scss'].includes(p.name))) {
+            const scss = (await import('rollup-plugin-scss')).default
+            plugins.push(scss( typeof options.css === 'string' ? { fileName: options.css } : options.css ))
         }
     }
     return {
